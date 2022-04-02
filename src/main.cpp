d5c900dfed9ad2f19964bc2ff162cb4cd9cf0f7e
Ohad Raviv <ohad.raviv@gmail.com>
2022-04-02 09:53:54 +0300

    Revert "looping to find the correct one - airwell 79"
    This reverts commit a519ac69627db883599b6ca347f62b2732918b5f.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,37 @@
 #include <Arduino.h>
 #include <IRremoteESP8266.h>
-#include <IRac.h>
-#include <IRutils.h>
+#include <IRsend.h>
+#include <ir_Electra.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
-const uint16_t kIrLed = 4;  // The ESP GPIO pin to use that controls the IR LED.
-IRac ac(kIrLed);  // Create a A/C object using GPIO to sending messages with.
+const uint16_t kIrLed = 4;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
+IRElectraAc ac(kIrLed);     // Set the GPIO used for sending messages.
+
+void printState() {
+  // Display the settings.
+  Serial.println("Electra A/C remote is in the following state:");
+  Serial.printf("  %s\n", ac.toString().c_str());
+
+  display.clearDisplay();
+	display.setCursor(0,0);
+	display.setTextSize(1);
+	display.println("state:");
+	display.print("power: ");
+  display.println(ac.getPower());
+	display.print("Temp: ");
+  display.println(ac.getTemp());
+	display.print("Mode: ");
+  display.println(ac.getMode());
+	display.print("Fan: ");
+  display.println(ac.getFan());
+	display.display();
+}
 
 void setup() {
+  ac.begin();
   Serial.begin(9600);
   	// initialize with the I2C addr 0x3C
 	display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
@@ -18,29 +39,16 @@ void setup() {
 	display.clearDisplay();
   delay(200);
 
-  // Set up what we want to send.
-  // See state_t, opmode_t, fanspeed_t, swingv_t, & swingh_t in IRsend.h for
-  // all the various options.
-  ac.next.protocol = decode_type_t::DAIKIN;  // Set a protocol to use.
-  ac.next.model = 1;  // Some A/Cs have different models. Try just the first.
-  ac.next.mode = stdAc::opmode_t::kCool;  // Run in cool mode initially.
-  ac.next.celsius = true;  // Use Celsius for temp units. False = Fahrenheit
-  ac.next.degrees = 25;  // 25 degrees.
-  ac.next.fanspeed = stdAc::fanspeed_t::kMedium;  // Start the fan at medium.
-  ac.next.swingv = stdAc::swingv_t::kOff;  // Don't swing the fan up or down.
-  ac.next.swingh = stdAc::swingh_t::kOff;  // Don't swing the fan left or right.
-  ac.next.light = false;  // Turn off any LED/Lights/Display that we can.
-  ac.next.beep = false;  // Turn off any beep from the A/C if we can.
-  ac.next.econo = false;  // Turn off any economy modes if we can.
-  ac.next.filter = false;  // Turn off any Ion/Mold/Health filters if we can.
-  ac.next.turbo = false;  // Don't use any turbo/powerful/etc modes.
-  ac.next.quiet = false;  // Don't use any quiet/silent/etc modes.
-  ac.next.sleep = -1;  // Don't set any sleep time or modes.
-  ac.next.clean = false;  // Turn off any Cleaning options if we can.
-  ac.next.clock = -1;  // Don't set any current time if we can avoid it.
-  ac.next.power = false;  // Initially start with the unit off.
-
-  Serial.println("Try to turn on & off every supported A/C type ...");
+  // Set up what we want to send. See ir_Electra.cpp for all the options.
+  Serial.println("Default state of the remote.");
+  printState();
+  Serial.println("Setting initial state for A/C.");
+  ac.off();
+  ac.setFan(kElectraAcFanLow);
+  ac.setMode(kElectraAcCool);
+  ac.setTemp(25);
+  //ac.setSwing(false);
+  printState();
 
 	// Display Text
 	display.setTextSize(1);
@@ -49,36 +57,43 @@ void setup() {
 	display.println("Hello world!");
 	display.display();
 
-  delay(1000);
 }
 
 void loop() {
-  // For every protocol the library has ...
-  for (int i = 1; i < kLastDecodeType; i++) {
-    decode_type_t protocol = (decode_type_t)i;
-    // If the protocol is supported by the IRac class ...
-    if (ac.isProtocolSupported(protocol)) {
-      Serial.println("Protocol " + String(protocol) + " / " +
-                     typeToString(protocol) + " is supported.");
-      
-  display.clearDisplay();
-	display.setCursor(0,0);
-	display.setTextSize(2);
-	display.println("Trying protocol:");
-  display.println(String(protocol));
-  display.println(typeToString(protocol));
-  display.display();
+  // Turn the A/C unit on
+  Serial.println("Turn on the A/C ...");
+  ac.on();
+  ac.send();
+  printState();
+  delay(15000);  // wait 15 seconds
+  // and set to cooling mode.
+  Serial.println("Set the A/C mode to cooling ...");
+  ac.setMode(kElectraAcCool);
+  ac.send();
+  printState();
+  delay(15000);  // wait 15 seconds
 
-      ac.next.protocol = protocol;  // Change the protocol used.
-      ac.next.power = true;  // We want to turn on the A/C unit.
-      Serial.println("Sending a message to turn ON the A/C unit.");
-      ac.sendAc();  // Have the IRac class create and send a message.
-      delay(5000);  // Wait 5 seconds.
-      ac.next.power = false;  // Now we want to turn the A/C off.
-      Serial.println("Send a message to turn OFF the A/C unit.");
-      ac.sendAc();  // Send the message.
-      delay(1000);  // Wait 1 second.
-    }
-  }
-  Serial.println("Starting from the begining again ...");
+  // Increase the fan speed.
+  Serial.println("Set the fan to high and the swing on ...");
+  ac.setFan(kElectraAcFanHigh);
+  //ac.setSwing(true);
+  ac.send();
+  printState();
+  delay(15000);
+
+  // Change to Fan mode, lower the speed, and stop the swing.
+  Serial.println("Set the A/C to fan only with a low speed, & no swing ...");
+  //ac.setSwing(false);
+  ac.setMode(kElectraAcFan);
+  ac.setFan(kElectraAcFanLow);
+  ac.send();
+  printState();
+  delay(15000);
+
+  // Turn the A/C unit off.
+  Serial.println("Turn off the A/C ...");
+  ac.off();
+  ac.send();
+  printState();
+  delay(15000);  // wait 15 seconds
 }
