508af180867c44f77a8fbe39e04c967c7147f090
Ohad Raviv <ohad.raviv@gmail.com>
2022-04-02 19:30:59 +0300

    adding a DHT

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,15 @@
 #include <ir_Airwell.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "DHTesp.h"
 
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
+DHTesp dht;
+//Define the DHT object
+int dhtPin = 13;//Define the dht pin
+float roomTemp = 0;
+float roomHumidity = 0;
 
 const uint16_t kIrLed = 4;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
 IRAirwellAc ac(kIrLed);     // Set the GPIO used for sending messages.
@@ -18,15 +25,23 @@ void printState() {
   display.clearDisplay();
 	display.setCursor(0,0);
 	display.setTextSize(1);
-	display.println("state:");
+	display.println("AC state");
 	display.print("power toggle: ");
   display.println(ac.getPowerToggle());
 	display.print("Temp: ");
   display.println(ac.getTemp());
 	display.print("Mode: ");
-  display.println(ac.getMode());
-	display.print("Fan: ");
+  display.print(ac.getMode());
+	display.print("     Fan: ");
   display.println(ac.getFan());
+  display.println();
+
+	display.println("Room State");
+  display.print("Tempratue: ");
+  display.println(roomTemp);
+	display.print("Humidity: ");
+  display.println(roomHumidity);
+
 	display.display();
 }
 
@@ -37,6 +52,7 @@ void setup() {
 	display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
   	// Clear the buffer.
 	display.clearDisplay();
+  dht.setup(dhtPin, DHTesp::DHT11);//Initialize the dht pin and dht object
   delay(200);
 
   // Set up what we want to send. See ir_Airwell.cpp for all the options.
@@ -64,6 +80,15 @@ void loop() {
   Serial.println("Turn on the A/C ...");
   ac.setPowerToggle(true);
   ac.send();
+
+  flag:TempAndHumidity newValues = dht.getTempAndHumidity();
+  if (dht.getStatus() != 0) {
+    goto flag;
+  }
+
+  roomTemp = newValues.temperature;
+  roomHumidity = newValues.humidity;
+
   printState();
   delay(60000);  // wait 15 seconds
 }
