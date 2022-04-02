c2590bd897c53b8f9d92e9075f3870fddea49d03
Ohad Raviv <ohad.raviv@gmail.com>
2022-04-02 10:02:00 +0300

    moving to Airwell - working

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,26 @@
 #include <Arduino.h>
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
-#include <ir_Electra.h>
+#include <ir_Airwell.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
 const uint16_t kIrLed = 4;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
-IRElectraAc ac(kIrLed);     // Set the GPIO used for sending messages.
+IRAirwellAc ac(kIrLed);     // Set the GPIO used for sending messages.
 
 void printState() {
   // Display the settings.
-  Serial.println("Electra A/C remote is in the following state:");
+  Serial.println("Airwell A/C remote is in the following state:");
   Serial.printf("  %s\n", ac.toString().c_str());
 
   display.clearDisplay();
 	display.setCursor(0,0);
 	display.setTextSize(1);
 	display.println("state:");
-	display.print("power: ");
-  display.println(ac.getPower());
+	display.print("power toggle: ");
+  display.println(ac.getPowerToggle());
 	display.print("Temp: ");
   display.println(ac.getTemp());
 	display.print("Mode: ");
@@ -39,13 +39,13 @@ void setup() {
 	display.clearDisplay();
   delay(200);
 
-  // Set up what we want to send. See ir_Electra.cpp for all the options.
+  // Set up what we want to send. See ir_Airwell.cpp for all the options.
   Serial.println("Default state of the remote.");
   printState();
   Serial.println("Setting initial state for A/C.");
-  ac.off();
-  ac.setFan(kElectraAcFanLow);
-  ac.setMode(kElectraAcCool);
+  //ac.off();
+  ac.setFan(kAirwellFanLow);
+  ac.setMode(kAirwellCool);
   ac.setTemp(25);
   //ac.setSwing(false);
   printState();
@@ -62,20 +62,20 @@ void setup() {
 void loop() {
   // Turn the A/C unit on
   Serial.println("Turn on the A/C ...");
-  ac.on();
+  ac.setPowerToggle(true);
   ac.send();
   printState();
   delay(15000);  // wait 15 seconds
   // and set to cooling mode.
   Serial.println("Set the A/C mode to cooling ...");
-  ac.setMode(kElectraAcCool);
+  ac.setMode(kAirwellCool);
   ac.send();
   printState();
   delay(15000);  // wait 15 seconds
 
   // Increase the fan speed.
   Serial.println("Set the fan to high and the swing on ...");
-  ac.setFan(kElectraAcFanHigh);
+  ac.setFan(kAirwellFanHigh);
   //ac.setSwing(true);
   ac.send();
   printState();
@@ -84,15 +84,15 @@ void loop() {
   // Change to Fan mode, lower the speed, and stop the swing.
   Serial.println("Set the A/C to fan only with a low speed, & no swing ...");
   //ac.setSwing(false);
-  ac.setMode(kElectraAcFan);
-  ac.setFan(kElectraAcFanLow);
+  ac.setMode(kAirwellFan);
+  ac.setFan(kAirwellFanLow);
   ac.send();
   printState();
   delay(15000);
 
   // Turn the A/C unit off.
   Serial.println("Turn off the A/C ...");
-  ac.off();
+  ac.setPowerToggle(false);
   ac.send();
   printState();
   delay(15000);  // wait 15 seconds
