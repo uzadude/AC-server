cf560d519c408e99ed6d343228167cbd77c8a389
Ohad Raviv <ohad.raviv@gmail.com>
2022-04-03 17:38:24 +0300

    adding simple web server from the examples

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,24 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include "DHTesp.h"
+#include <ESPmDNS.h>
+#include <WebServer.h>
+#include <WiFi.h>
+#include <Update.h>
+#include <WiFiUdp.h>
+#include <ArduinoJson.h>
+#include <SPIFFS.h>
+
+#define AUTO_MODE kAirwellAuto
+#define COOL_MODE kAirwellCool
+#define DRY_MODE kAirwellDry
+#define HEAT_MODE kAirwellHeat
+#define FAN_MODE kAirwellFan
+
+#define FAN_AUTO kAirwellFanAuto
+#define FAN_MIN kAirwellFanLow
+#define FAN_MED kAirwellFanMedium
+#define FAN_HI kAirwellFanHigh
 
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
@@ -13,14 +31,112 @@ DHTesp dht;
 int dhtPin = 13;//Define the dht pin
 float roomTemp = 0;
 float roomHumidity = 0;
+unsigned long lastPrintMS = 0;
 
 const uint16_t kIrLed = 4;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
 IRAirwellAc ac(kIrLed);     // Set the GPIO used for sending messages.
+
+struct state {
+  uint8_t temperature = 22, fan = 0, operation = 0;
+  bool powerStatus;
+};
+
+File fsUploadFile;
+state acState;
+
+char deviceName[] = "AC Remote Control";
+const char* ssid = "";
+const char* password = "";
+
+WebServer server(80);
+
+String getContentType(String filename) {
+  // convert the file extension to the MIME type
+  if (filename.endsWith(".html")) return "text/html";
+  else if (filename.endsWith(".css")) return "text/css";
+  else if (filename.endsWith(".js")) return "application/javascript";
+  else if (filename.endsWith(".ico")) return "image/x-icon";
+  else if (filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
+
+bool handleFileRead(String path) {
+  //  send the right file to the client (if it exists)
+  // Serial.println("handleFileRead: " + path);
+  if (path.endsWith("/")) path += "index.html";
+  // If a folder is requested, send the index file
+  String contentType = getContentType(path);
+  // Get the MIME type
+  String pathWithGz = path + ".gz";
+  if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)) {
+    // If the file exists, either as a compressed archive, or normal
+    // If there's a compressed version available
+    if (SPIFFS.exists(pathWithGz))
+      path += ".gz";  // Use the compressed verion
+    File file = SPIFFS.open(path, "r");
+    //  Open the file
+    server.streamFile(file, contentType);
+    //  Send it to the client
+    file.close();
+    // Close the file again
+    // Serial.println(String("\tSent file: ") + path);
+    return true;
+  }
+  // Serial.println(String("\tFile Not Found: ") + path);
+  // If the file doesn't exist, return false
+  return false;
+}
+
+void handleFileUpload() {  // upload a new file to the SPIFFS
+  HTTPUpload& upload = server.upload();
+  if (upload.status == UPLOAD_FILE_START) {
+    String filename = upload.filename;
+    if (!filename.startsWith("/")) filename = "/" + filename;
+    // Serial.print("handleFileUpload Name: "); //Serial.println(filename);
+    fsUploadFile = SPIFFS.open(filename, "w");
+    // Open the file for writing in SPIFFS (create if it doesn't exist)
+    filename = String();
+  } else if (upload.status == UPLOAD_FILE_WRITE) {
+    if (fsUploadFile)
+      fsUploadFile.write(upload.buf, upload.currentSize);
+      // Write the received bytes to the file
+  } else if (upload.status == UPLOAD_FILE_END) {
+    if (fsUploadFile) {
+      // If the file was successfully created
+      fsUploadFile.close();
+      // Close the file again
+      // Serial.print("handleFileUpload Size: ");
+      // Serial.println(upload.totalSize);
+      server.sendHeader("Location", "/success.html");
+      // Redirect the client to the success page
+      server.send(303);
+    } else {
+      server.send(500, "text/plain", "500: couldn't create file");
+    }
+  }
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
 
 void printState() {
   // Display the settings.
   Serial.println("Airwell A/C remote is in the following state:");
   Serial.printf("  %s\n", ac.toString().c_str());
+  Serial.print("WiFi: ");
+  Serial.println(WiFi.localIP()); // Print the IP address
 
   display.clearDisplay();
 	display.setCursor(0,0);
@@ -55,6 +171,135 @@ void setup() {
   dht.setup(dhtPin, DHTesp::DHT11);//Initialize the dht pin and dht object
   delay(200);
 
+  Serial.println("mounting SPIFFS...");
+    if (!SPIFFS.begin()) {
+    // Serial.println("Failed to mount file system");
+    return;
+  }
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  
+  if(WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("WiFi Connect Failed! Rebooting...");
+    delay(1000);
+    ESP.restart();
+  }  
+  Serial.println(WiFi.localIP()); // Print the IP address
+
+  server.on("/state", HTTP_PUT, []() {
+    DynamicJsonDocument root(1024);
+    DeserializationError error = deserializeJson(root, server.arg("plain"));
+    if (error) {
+      Serial.println("error!");
+      server.send(404, "text/plain", "FAIL. " + server.arg("plain"));
+    } else {
+      if (root.containsKey("temp")) {
+        acState.temperature = (uint8_t) root["temp"];
+      }
+
+      if (root.containsKey("fan")) {
+        acState.fan = (uint8_t) root["fan"];
+      }
+
+      if (root.containsKey("power")) {
+        acState.powerStatus = root["power"];
+      }
+
+      if (root.containsKey("mode")) {
+        acState.operation = root["mode"];
+      }
+
+      String output;
+      serializeJson(root, output);
+      server.send(200, "text/plain", output);
+      Serial.println("got request:");
+      Serial.println(output);
+
+      delay(200);
+
+      if (acState.powerStatus) {
+        ac.setPowerToggle(true);
+        ac.setTemp(acState.temperature);
+        if (acState.operation == 0) {
+          ac.setMode(AUTO_MODE);
+          ac.setFan(FAN_AUTO);
+          acState.fan = 0;
+        } else if (acState.operation == 1) {
+          ac.setMode(COOL_MODE);
+        } else if (acState.operation == 2) {
+          ac.setMode(DRY_MODE);
+        } else if (acState.operation == 3) {
+          ac.setMode(HEAT_MODE);
+        } else if (acState.operation == 4) {
+          ac.setMode(FAN_MODE);
+        }
+
+        if (acState.operation != 0) {
+          if (acState.fan == 0) {
+            ac.setFan(FAN_AUTO);
+          } else if (acState.fan == 1) {
+            ac.setFan(FAN_MIN);
+          } else if (acState.fan == 2) {
+            ac.setFan(FAN_MED);
+          } else if (acState.fan == 3) {
+            ac.setFan(FAN_HI);
+          }
+        }
+      } else {
+        ac.setPowerToggle(true);
+      }
+      ac.send();
+    }
+  });
+
+  server.on("/file-upload", HTTP_POST,
+  // if the client posts to the upload page
+  []() {
+    // Send status 200 (OK) to tell the client we are ready to receive
+    server.send(200);
+  },
+  handleFileUpload);  // Receive and save the file
+
+  server.on("/file-upload", HTTP_GET, []() {
+    // if the client requests the upload page
+
+    String html = "<form method=\"post\" enctype=\"multipart/form-data\">";
+    html += "<input type=\"file\" name=\"name\">";
+    html += "<input class=\"button\" type=\"submit\" value=\"Upload\">";
+    html += "</form>";
+    server.send(200, "text/html", html);
+  });
+
+  server.on("/", []() {
+    server.sendHeader("Location", String("ui.html"), true);
+    server.send(302, "text/plain", "");
+  });
+
+  server.on("/state", HTTP_GET, []() {
+    DynamicJsonDocument root(1024);
+    root["mode"] = acState.operation;
+    root["fan"] = acState.fan;
+    root["temp"] = acState.temperature;
+    root["power"] = acState.powerStatus;
+    String output;
+    serializeJson(root, output);
+    server.send(200, "text/plain", output);
+  });
+
+
+  server.on("/reset", []() {
+    server.send(200, "text/html", "reset");
+    delay(100);
+    ESP.restart();
+  });
+
+  server.serveStatic("/", SPIFFS, "/", "max-age=86400");
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+
   // Set up what we want to send. See ir_Airwell.cpp for all the options.
   Serial.println("Default state of the remote.");
   printState();
@@ -76,19 +321,19 @@ void setup() {
 }
 
 void loop() {
-  // Turn the A/C unit on
-  Serial.println("Turn on the A/C ...");
-  ac.setPowerToggle(true);
-  ac.send();
+  server.handleClient();
 
-  flag:TempAndHumidity newValues = dht.getTempAndHumidity();
-  if (dht.getStatus() != 0) {
-    goto flag;
+  if (millis() - lastPrintMS > 10000) {
+    flag:TempAndHumidity newValues = dht.getTempAndHumidity();
+    if (dht.getStatus() != 0) {
+      goto flag;
+    }
+
+    roomTemp = newValues.temperature;
+    roomHumidity = newValues.humidity;
+
+    printState();
+    lastPrintMS = millis();
   }
-
-  roomTemp = newValues.temperature;
-  roomHumidity = newValues.humidity;
-
-  printState();
-  delay(60000);  // wait 15 seconds
+  delay(1000);
 }
